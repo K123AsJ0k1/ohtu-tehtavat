92fa1bab0162434f907b10b14d7e85366e0cfdd8
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2021-03-18 18:00:34 +0200

    Tehtava 1 asiat

--- viikko3/tehtava1/src/main/java/ohtu/Player.java
+++ viikko3/tehtava1/src/main/java/ohtu/Player.java
@@ -3,18 +3,37 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String team;
+    private int goals;
+    private int assists;
 
-    public void setName(String name) {
+    public void setName(String name, String team, int goals, int assists) {
         this.name = name;
+        this.team = team;
+        this.goals = goals;
+        this.assists = assists;
+              
     }
 
     public String getName() {
         return name;
     }
+    
+    public String getTeam() {
+        return team;
+    }
+    
+    public int getGoals() {
+        return goals;
+    }
+    
+    public int getAssists() {
+        return assists;
+    }
 
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists ;
     }
       
 }
