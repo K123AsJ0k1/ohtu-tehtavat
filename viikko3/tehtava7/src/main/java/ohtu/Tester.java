48b11d6e4cb04a13af2fa76ba232a6c27af01521
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2021-03-19 18:05:48 +0200

    Tehtava 8

--- viikko3/tehtava7/src/main/java/ohtu/Tester.java
+++ viikko3/tehtava7/src/main/java/ohtu/Tester.java
@@ -8,29 +8,83 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Chromedriver/chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
         
         sleep(2);
-        
+        /*
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
         sleep(2);
-
+        //yksi salasanat.txt tiedostossa olevista käyttäjistä
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("testi");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("1sala2ana"); // sala2ana
         element = driver.findElement(By.name("login"));
         
         sleep(2);
         element.submit();
 
         sleep(3);
+        */
+        /*
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
         
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("kokeilu");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("kokeiluSalasana2");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("kokeiluSalasana2");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+
         driver.quit();
+        */
+
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("kokeilu4");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("kokeiluSalasana3");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("kokeiluSalasana3");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        sleep(2);
+
+        driver.quit();
+
+       
     }
     
     private static void sleep(int n){
